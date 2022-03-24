1346b64466c5d6a9291ce9d7a5506825692451f8
Gleb Danshin <glebd79@gmail.com>
2022-03-24 20:15:20 +0300

    Added tests for constructors

--- modules/complex-number/test/test_DANSHIN_GLEB_complex_number.cpp
+++ modules/complex-number/test/test_DANSHIN_GLEB_complex_number.cpp
@@ -3,3 +3,16 @@
 #include <gtest/gtest.h>
 
 #include "include/complex_number.h"
+
+TEST(DanshinGlebComplexNumberTest, DefaultConstructor) {
+  ASSERT_NO_THROW(ComplexNumber());
+}
+
+TEST(DanshinGlebComplexNumberTest, ParameterizedConstructor) {
+  ASSERT_NO_THROW(ComplexNumber(1.0, 2.0));
+}
+
+TEST(DanshinGlebComplexNumberTest, CopyConstructor) {
+  auto origin = ComplexNumber();
+  ASSERT_NO_THROW(ComplexNumber(number_1));
+}
