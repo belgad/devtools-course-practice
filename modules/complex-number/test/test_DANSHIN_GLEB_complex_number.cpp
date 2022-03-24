2c17f7f94a544dd27317bf902d392f07db9b0b34
Gleb Danshin <glebd79@gmail.com>
2022-03-24 20:21:32 +0300

    Added tests for assignment and equality operators

--- modules/complex-number/test/test_DANSHIN_GLEB_complex_number.cpp
+++ modules/complex-number/test/test_DANSHIN_GLEB_complex_number.cpp
@@ -16,3 +16,27 @@ TEST(DanshinGlebComplexNumberTest, CopyConstructor) {
   auto origin = ComplexNumber();
   ASSERT_NO_THROW(ComplexNumber(number_1));
 }
+
+TEST(DanshinGlebComplexNumberTest, AssignmentOperator) {
+  auto origin = ComplexNumber(1.0, 2.0);
+  auto copy = ComplexNumber();
+  ASSERT_NO_THROW(copy = origin);
+}
+
+TEST(DanshinGlebComplexNumberTest, EqualityOperator) {
+  auto first = ComplexNumber(1.0, 2.0);
+  auto second = ComplexNumber(1.0, 2.0);
+  ASSERT_EQ(first, second);
+}
+
+TEST(DanshinGlebComplexNumberTest, CopyEqualsToOrigin) {
+  auto origin = ComplexNumber(1.0, 2.0);
+  auto copy = origin;
+  ASSERT_EQ(origin, copy);
+}
+
+TEST(DanshinGlebComplexNumberTest, InequalityOperator) {
+  auto first = ComplexNumber(1.0, 2.0);
+  auto second = ComplexNumber(3.0, 4.0);
+  ASSERT_NE(first, second);
+}
