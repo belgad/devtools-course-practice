4e5cf8990a51e44542ee0ba66a04d93967fb992a
Gleb Danshin <glebd79@gmail.com>
2022-04-18 19:41:45 +0300

    Graph class constructors added

--- modules/graph_diameter/include/graph_diameter.h
+++ modules/graph_diameter/include/graph_diameter.h
@@ -1,1 +1,19 @@
 // Copyright 2022 Gleb "belgad" Danshin
+#ifndef MODULES_GRAPH_DIAMETER_INCLUDE_GRAPH_DIAMETER_H_
+#define MODULES_GRAPH_DIAMETER_INCLUDE_GRAPH_DIAMETER_H_
+
+#include <vector>
+
+class Graph {
+  using Vector = std::vector<int>;
+  using Matrix = std::vector<Vector>;
+ public:
+  Graph();
+  Graph(size_t);
+  Graph(Matrix&);
+ private:
+  size_t graph_size_;
+  Matrix weights_;
+};
+
+#endif // MODULES_GRAPH_DIAMETER_INCLUDE_GRAPH_DIAMETER_H_
--- modules/graph_diameter/src/graph_diameter.cpp
+++ modules/graph_diameter/src/graph_diameter.cpp
@@ -1,3 +1,15 @@
 // Copyright 2022 Gleb "belgad" Danshin
-
 #include "include/graph_diameter.h"
+
+#include <vector>
+
+Graph::Graph()
+    : graph_size_(0),
+      weights_(Matrix(0, Vector(0))) {}
+
+Graph::Graph(size_t graph_size)
+    : graph_size_(graph_size), weights_(
+      Matrix(graph_size, Vector(graph_size, 0))) {}
+
+Graph::Graph(Matrix& weights)
+    : graph_size_(weights.size()), weights_(weights) {}
